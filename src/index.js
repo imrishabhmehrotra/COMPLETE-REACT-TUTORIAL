08d56afeca7d43d574daf2244070df0c30252ebb
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2023-12-09 00:50:38 +0530

    created webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const child1 = React.createElement('p', {}, "I am a child 1")
 const child2 = React.createElement('p', {}, "I am a child 2")
 
 const div = React.createElement('div', {className: 'text'}, [child1, child2])
+//const div = <div className="text">hello 2</div>
 
 console.log(div)
 const root = ReactDOM.createRoot(document.getElementById('root'))
