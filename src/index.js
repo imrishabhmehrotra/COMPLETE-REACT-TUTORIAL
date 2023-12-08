89372f9227f7145be6b9c72189ea8944498a02fe
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2023-12-09 01:57:10 +0530

    JSX, React elements, React components and Function Calling & Rendering.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const child1 = React.createElement('p', {}, "I am a child 1")
-const child2 = React.createElement('p', {}, "I am a child 2")
+//const child1 = React.createElement('p', {}, "I am a child 1")
+//const child2 = React.createElement('p', {}, "I am a child 2")
 
-const div = React.createElement('div', {className: 'text'}, [child1, child2])
-//const div = <div className="text">hello 2</div>
+//const div = React.createElement('div', {className: 'text'}, [child1, child2])
 
-console.log(div)
-const root = ReactDOM.createRoot(document.getElementById('root'))
+//here div is not a component but it is react element.
 
-root.render(div)
+//Conditional rendering ternary operators
+const isMorning = false;
+const morningText = "good morning user";
+const afternoonText = "Good afternoon user";
+
+function handleClick() {
+    console.log("i was clicked")
+}
+
+//conditional rendering
+const morningElement = (
+  <div>
+    {morningText} <span onClick = {handleClick}>it is 8am</span>
+  </div>
+);
+const afternoonElement = <div>{afternoonText} it is 6pm</div>;
+const greetingElement = isMorning ? morningElement : afternoonElement;
+
+//const div = <div className="text">{isMorning ? morningText : afternoonText}</div>
+
+// In JavaScript style style it can be written as:
+//const GreetingComponent = () => <>
+//  {greetingElement}i am inside component</>
+
+const GreetingComponent = function () {
+  return <div>{greetingElement}i am inside component
+  </div>;
+};
+console.log(GreetingComponent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(GreetingComponent());
