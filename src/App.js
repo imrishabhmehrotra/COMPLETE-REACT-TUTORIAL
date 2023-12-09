ca3933f5b84558c498d24b58ddae95959689fec2
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2023-12-09 11:15:54 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import Button from './Button'
+import React, { useState } from "react";
+import Button from "./Button";
+import Text from "./Text";
 
 const App = () => {
-    const clickAction = () => {
-        console.log("I was clicked from parent")
-    }
-    return <Button clickAction={handleClickAction}>Click Me</Button>
-}
+  const [data, setData] = useState([
+    {
+      id: "a",
+      text: "text 1",
+    },
+    { id: "b", text: "text 2" },
+    { id: "c", text: "text 3" },
+    { id: "d", text: "text 4" },
+  ]);
+  const handleClickAction = () => {
+    console.log("I was clicked from parent");
+  };
+  return;
+  <>
+    {data.map((item, index) => (
+      <Text key={index}>{item.text}</Text>
+    ))}
+  </>;
+};
 
-export default App
+export default App;
